9704ab1407083bebe657d4b2994f737123f78adf
brunosc10699 <brunosc10699@gmail.com>
2021-06-18 17:52:29 -0300

    BookService class created

---
+++ src/main/java/com/bruno/project/services/BookService.java
@@ -0,0 +1,12 @@
+package com.bruno.project.services;
+
+import com.bruno.project.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+}
