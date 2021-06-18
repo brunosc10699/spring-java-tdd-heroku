d003420578f06cbf1eb097da5dabb9564eadd335
brunosc10699 <brunosc10699@gmail.com>
2021-06-18 17:50:52 -0300

    AuthorRepository class created

---
+++ src/main/java/com/bruno/project/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.bruno.project.repositories;
+
+import com.bruno.project.entities.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
