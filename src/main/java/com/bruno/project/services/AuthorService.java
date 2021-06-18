a18db885d617dc91490ea870d98d5670d8b2259f
brunosc10699 <brunosc10699@gmail.com>
2021-06-18 17:53:59 -0300

    AuthorService class created

---
+++ src/main/java/com/bruno/project/services/AuthorService.java
@@ -0,0 +1,12 @@
+package com.bruno.project.services;
+
+import com.bruno.project.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorService {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+}
