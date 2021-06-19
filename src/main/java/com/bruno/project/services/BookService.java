3ab946639c5a1a699d5a59f0ad856a18e942db53
brunosc10699 <brunosc10699@gmail.com>
2021-06-18 22:36:30 -0300

    The written test for findAll() service method succeeded

--- src/main/java/com/bruno/project/services/BookService.java
+++ src/main/java/com/bruno/project/services/BookService.java
@@ -1,12 +1,23 @@
 package com.bruno.project.services;
 
+import com.bruno.project.dto.BookDTO;
 import com.bruno.project.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
+
+    public List<BookDTO> findAll() {
+        return bookRepository.findAll().stream().map(BookDTO::new).collect(Collectors.toList());
+    }
 }
--- src/test/java/com/bruno/project/services/BookServiceTest.java
+++ src/test/java/com/bruno/project/services/BookServiceTest.java
@@ -1,18 +1,46 @@
 package com.bruno.project.services;
 
 import com.bruno.project.dto.BookDTO;
+import com.bruno.project.entities.Author;
 import com.bruno.project.entities.Book;
+import com.bruno.project.enums.BookGenre;
 import com.bruno.project.repositories.BookRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
 
+    private List<Book> books = new ArrayList<>();
+
+    private Author author = new Author(
+            1l,
+            "Jo Nesbø",
+            LocalDate.parse("1960-03-29"),
+            "jonesbo@jonesbo.com",
+            null,
+            "Jo Nesbø (Norwegian: born 29 March 1960) is a Norwegian writer, musician, economist, and former soccer player and reporter. More than 3 million copies of his novels had been sold in Norway as of March 2014; his work has been translated into over 50 languages, and by 2021 had sold some 50 million copies worldwide. Known primarily for his crime novels featuring Inspector Harry Hole, Nesbø is also the main vocalist and songwriter for the Norwegian rock band Di Derre. In 2007 he released his first children's book, Doktor Proktors Prompepulver (English translation: Doctor Proctor's Fart Powder). The 2011 film Headhunters is based on Nesbø's novel Hodejegerne (The Headhunters).",
+            null,
+            books
+    );
+
+    private List<Author> authors = new ArrayList<>();
+
     private Book expectedBook = new Book(
-            1,
+            1L,
             "978-0099520320",
             "The Bat",
             432,
@@ -20,8 +48,8 @@ public class BookServiceTest {
             "1997",
             "Harvill Secker",
             null,
-            6,
-            "Jo Nesbø"
+            BookGenre.toEnum(6),
+            authors
     );
 
     private BookDTO givenBook = new BookDTO(expectedBook);
@@ -31,4 +59,12 @@ public class BookServiceTest {
 
     @InjectMocks
     private BookService bookService;
+
+    @Test
+    void whenFindAllIsCalledThenReturnAListOfBooks() {
+        when(bookRepository.findAll()).thenReturn(Collections.singletonList(expectedBook));
+        List<BookDTO> list = bookService.findAll();
+        assertThat(list, is(not(empty())));
+        assertThat(list.get(0), is(equalTo(givenBook)));
+    }
 }
