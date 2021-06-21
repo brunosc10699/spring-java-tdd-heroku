3c0c9ab02d289cab88573ecaf13598e7c19faa4c
brunosc10699 <brunosc10699@gmail.com>
2021-06-21 13:49:08 -0300

    BookResourceTest class created and ready to start tests

---
+++ src/test/java/com/bruno/project/resources/BookResourceTest.java
@@ -0,0 +1,71 @@
+package com.bruno.project.resources;
+
+import com.bruno.project.dto.BookDTO;
+import com.bruno.project.entities.Author;
+import com.bruno.project.entities.Book;
+import com.bruno.project.enums.BookGenre;
+import com.bruno.project.resource.BookResource;
+import com.bruno.project.services.BookService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class BookResourceTest {
+
+    private Author author = new Author(
+            1l,
+            "Jo Nesbø",
+            LocalDate.parse("1960-03-29"),
+            "jonesbo@jonesbo.com",
+            null,
+            "Jo Nesbø (Norwegian: born 29 March 1960) is a Norwegian writer, musician, economist, and former soccer player and reporter. More than 3 million copies of his novels had been sold in Norway as of March 2014; his work has been translated into over 50 languages, and by 2021 had sold some 50 million copies worldwide. Known primarily for his crime novels featuring Inspector Harry Hole, Nesbø is also the main vocalist and songwriter for the Norwegian rock band Di Derre. In 2007 he released his first children's book, Doktor Proktors Prompepulver (English translation: Doctor Proctor's Fart Powder). The 2011 film Headhunters is based on Nesbø's novel Hodejegerne (The Headhunters).",
+            null,
+            null
+    );
+
+    private List<Author> authors = new ArrayList<>();
+
+    private Book expectedBook = new Book(
+            1L,
+            "978-0099520320",
+            "The Bat",
+            432,
+            "English",
+            "1997",
+            "Harvill Secker",
+            null,
+            BookGenre.toEnum(6),
+            authors
+    );
+
+    private BookDTO givenBook = new BookDTO(expectedBook);
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private BookService bookService;
+
+    @InjectMocks
+    private BookResource bookResource;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(bookResource)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
+                .build();
+    }
+
+
+}
