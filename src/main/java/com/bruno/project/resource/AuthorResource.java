fb1763a9963739b50d46cbc12a268e10bee0877b
brunosc10699 <brunosc10699@gmail.com>
2021-06-21 13:37:29 -0300

    AuthorResource class created

--- src/main/java/com/bruno/project/resource/AuthorResource.java
+++ src/main/java/com/bruno/project/resource/AuthorResource.java
@@ -1,15 +1,15 @@
 package com.bruno.project.resource;
 
-import com.bruno.project.services.BookService;
+import com.bruno.project.services.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/v1/books")
-public class BookResource {
+@RequestMapping(value = "/api/v1/authors")
+public class AuthorResource {
 
     @Autowired
-    private BookService bookService;
-
+    private AuthorService authorService;
+    
 }
